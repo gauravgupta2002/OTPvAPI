c9e78023b8e3557a03c13d4beb7caf688a7e8a76
gauravgupta2002 <gauravgupta200222@gmail.com>
2022-08-02 15:47:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/otpvapi/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.otpvapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText edtPhone;
+    private Button generateOTPBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtPhone = findViewById(R.id.idEdtPhoneNumber);
+        generateOTPBtn = findViewById(R.id.idBtnGetOtp);
+
+
+        generateOTPBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(edtPhone.getText().toString().trim().isEmpty())
+                {
+                    edtPhone.setError("Enter Phone No");
+                }else{
+
+                    int randomPin   =(int) (Math.random()*9000)+1000;
+                    String otp  = String.valueOf(randomPin);
+                   // postDataUsingVolley(edtPhone.getText().toString(), otp);
+                    sendOTP(otp);
+
+                    Intent i = new Intent(MainActivity.this, verify.class);
+                    i.putExtra("otp",otp.trim());
+                    startActivity(i);
+                    finish();
+
+
+                }
+            }
+        });
+
+    }
+
+    private void sendOTP(String otp) {
+        try {
+            // Construct data
+            String apiKey = "apikey=" + "hkvjTUuwqXgMHsZ1";
+            //Random random = new Random();
+            //randonnumber=random.nextInt(99999);
+            String message = "&message=" + "Hey, Your OTP is " +otp;
+            String senderid = "&sender=" + "TXTLCL";
+            String numbers = "&numbers=" +edtPhone;
+            String format = "&format" +"json";
+            // Send data
+            HttpURLConnection conn = (HttpURLConnection) new URL("http://msg.mtalkz.com/V2/http-api-post.php?").openConnection();
+            String data = apiKey + numbers + message + senderid+format;
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Length", Integer.toString(data.length()));
+            conn.getOutputStream().write(data.getBytes("UTF-8"));
+            final BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            final StringBuffer stringBuffer = new StringBuffer();
+            String line;
+            while ((line = rd.readLine()) != null) {
+                stringBuffer.append(line);
+            }
+            rd.close();
+        } catch (Exception e) {
+            System.out.println("Error SMS "+e);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/otpvapi/verify.java
@@ -0,0 +1,58 @@
+package com.example.otpvapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Random;
+
+public class verify extends AppCompatActivity {
+
+    private EditText edtOTP;
+    private String otp_text;
+    private Button verifyOTPBtn;
+   // int randonnumber;
+    String otp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify);
+
+        edtOTP = findViewById(R.id.idEdtOtp);
+        verifyOTPBtn = findViewById(R.id.idBtnVerify);
+
+        otp = getIntent().getStringExtra("otp");
+        verifyOTPBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                verifyOTP(otp);
+            }
+        });
+    }
+
+    public void verifyOTP(String randomnumber) {
+        //Toast.makeText(verify.this,"Verify Button",Toast.LENGTH_LONG).show();
+        otp_text = edtOTP.getText().toString().trim();
+        if(edtOTP.equals(String.valueOf(randomnumber)))
+        {
+            Toast.makeText(verify.this,"user login in successfully",Toast.LENGTH_LONG).show();
+            finish();
+            Intent mainactivity = new Intent(verify.this,login.class);
+            startActivity(mainactivity);
+        }
+        else{
+            Toast.makeText(verify.this,"Invalid OTP, Please Try Again",Toast.LENGTH_LONG).show();
+        }
+    }
+}
